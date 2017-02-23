49e242a273683968b8e7854e0975c903f8a377e8
Hopding <andrew.dillon.j@gmail.com>
2017-02-23 09:24:55 -0600

    Added TokenInfoPanel; Cardified language and tokens, and code area; Added snippet name field

--- src/SnippetArea.jsx
+++ src/SnippetArea.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
+import { Card, CardText } from 'material-ui/Card';
 
 const SnippetArea = () => (
-  <TextField
-    fullWidth={true}
-    floatingLabelFixed={true}
-    floatingLabelText="Enter code here"
-    multiLine={true}
-    name="snippetArea"
-    rows={30}
-  />
+  <Card>
+    <CardText>
+    <TextField
+      name="snippetName"
+      hintText="Snippet Name"
+    />
+    <TextField
+      fullWidth={true}
+      floatingLabelFixed={true}
+      floatingLabelText="Enter code here"
+      multiLine={true}
+      name="snippetArea"
+      rows={30}
+    />
+    </CardText>
+  </Card>
 );
 
 export default SnippetArea;
---
+++ src/TokenInfoPanel.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Card, CardTitle, CardText } from 'material-ui/Card';
+import { Tabs, Tab } from 'material-ui/Tabs';
+
+const TokenInfoPanel = () => (
+  <Card>
+    <CardTitle
+      title="Info Panel"
+    />
+    <CardText>General info about the selected token goes here</CardText>
+    <Tabs>
+      <Tab label="Annotation">
+        <CardText>Here is some annotation info about the token.</CardText>
+      </Tab>
+      <Tab label="SyntaxDB">
+        <CardText>Here is some info from SyntaxDB about the token.</CardText>
+      </Tab>
+    </Tabs>
+  </Card>
+);
+
+export default TokenInfoPanel;
