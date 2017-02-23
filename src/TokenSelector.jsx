31c4dbaee010d56f46bc6241082c5905c066e1ae
JMH <jheld135@gmail.com>
2017-02-23 09:55:02 -0600

    Merged origin/master

--- src/TokenSelector.jsx
+++ src/TokenSelector.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { List, ListItem } from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 import Checkbox from 'material-ui/Checkbox';
@@ -7,29 +7,39 @@ class TokenSelector extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedTokens: [],
+      selectedTokens: {},
     };
+    this.makeListItems = this.makeListItems.bind(this);
   }
+
+  makeListItems() {
+    return this.props.tokenTypes.map((tokenType, index) => {
+      return (
+        <ListItem
+          key={`${tokenType.text}-index`}
+          leftCheckbox={<Checkbox />}
+          onClick={(ev) => {console.log(ev.target.value);}}
+          primaryText={tokenType.text}
+        />
+      );
+    });
+  }
+
   render() {
     return (
       <List>
         <Subheader>Token types to highlight</Subheader>
-        <ListItem
-          leftCheckbox={<Checkbox />}
-          primaryText="Functions"
-        />
-        <ListItem
-          leftCheckbox={<Checkbox />}
-          primaryText="Variables"
-        />
-        <ListItem
-          leftCheckbox={<Checkbox />}
-          primaryText="For loop"
-        />
+        { this.makeListItems() }
       </List>
     );
   }
-
 }
+
+TokenSelector.propTypes = {
+  tokenTypes: PropTypes.arrayOf(PropTypes.shape({
+    text: PropTypes.string,
+  })).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default TokenSelector;
