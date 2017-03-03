38264b836b2537f31efa8b8968d9babbd1b38e1d
Dane Johnson <dane_johnson@ymail.com>
2017-03-03 11:15:42 -0600

    Added pretty names

--- src/util/tokens.js
+++ src/util/tokens.js
@@ -1,20 +1,27 @@
 const prettyTokenNameMap = {
-  'for_stmt' :    'For Loops',
-  'if_stmt':      'If Statements',
-  'number':       'Numbers',
-  'expr':         'Expressions',
-  'str':          'Strings',
-  'atom':         'Atoms',
-  'expr_stmt':    'Expression Statements',
-  'arglist':      'Argument Lists',
-  'argument':     'Arguments',
-  'integer':      'Integers',
-  'funcdef':      'Function Definitions',
-  'parameters':   'Parameters',
-  'classdef':     'Class Definitions',
-  'try_stmt':     'Try Statements',
-  'pass_stmt':    'Pass Statements',
-  'except_clause':'Except Clauses',
+  'for_stmt' :      'For Loops',
+  'if_stmt':        'If Statements',
+  'number':         'Numbers',
+  'expr':           'Expressions',
+  'str':            'Strings',
+  'atom':           'Atoms',
+  'expr_stmt':      'Expression Statements',
+  'arglist':        'Argument Lists',
+  'argument':       'Arguments',
+  'integer':        'Integers',
+  'funcdef':        'Function Definitions',
+  'parameters':     'Parameters',
+  'classdef':       'Class Definitions',
+  'try_stmt':       'Try Statements',
+  'pass_stmt':      'Pass Statements',
+  'except_clause':  'Except Clauses',
+  'while_stmt':     'While Loops',
+  'and_expr':       'And',
+  'comp_op':        'Comparison Operator',
+  'dictorsetmaker': 'Dictionary',
+  'import_name':    'Import Statement',
+  'return_stmt':    'Return Statement',
+  'augassign':      'Augmented Assignment'
 }
 
 /* Maps AST token types to pretty strings for displaying */
@@ -27,7 +34,7 @@ export function getTokenCount(node, map) {
   if (node.type === undefined) return;
   if (map[node.type] === undefined)
     map[node.type] = 1;
-  else 
+  else
     map[node.type] += 1;
   node.children.forEach(child => getTokenCount(child, map));
 }
