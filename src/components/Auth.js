6616ef21d4ab964c129b17f796bbaeb855377536
Dane Johnson <dane_johnson@ymail.com>
2017-03-07 11:13:43 -0600

    Auth now passes code to backend

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import axios from 'axios'
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
 
-const Auth = ({location: {query: {code}}}) => (
-  null
-)
+//Code is passed in from Github
+const Auth = ({location: {query: {code}}}) => {
+  axios.post('/api/auth',{
+    code,
+  })
+  return null
+}
 
 export default Auth
