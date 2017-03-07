e15e561fbae9deffe369a8e07c3b244e2cf3bba3
Dane Johnson <dane_johnson@ymail.com>
2017-03-07 12:37:08 -0600

    Added auth communitcation between front and backend

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import axios from 'axios'
-
-const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
-
+import cookie from 'react-cookie'
 //Code is passed in from Github
 const Auth = ({location: {query: {code}}}) => {
   axios.post('/api/auth',{
     code,
+  }).then((res) => {
+    //Code was accepted, the token should be in the response data
+    const { token } = res.data
+    cookie.save('token', token)
+    location = '/'
+  }).catch((err) => {
+    console.error(err)
+    location = '/'
   })
   return null
 }
