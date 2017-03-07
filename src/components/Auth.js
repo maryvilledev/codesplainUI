d7a2cd3a0541ee1283523a3dc910b2078f9f063d
Dane Johnson <dane_johnson@ymail.com>
2017-03-07 12:46:28 -0600

    refactored Auth to a class implementation

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,19 +2,31 @@ import React from 'react'
 import axios from 'axios'
 import cookie from 'react-cookie'
 //Code is passed in from Github
-const Auth = ({location: {query: {code}}}) => {
-  axios.post('/api/auth',{
-    code,
-  }).then((res) => {
-    //Code was accepted, the token should be in the response data
-    const { token } = res.data
-    cookie.save('token', token)
-    location = '/'
-  }).catch((err) => {
-    console.error(err)
-    location = '/'
-  })
-  return null
+class Auth extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      waiting: true,
+      authenticated: false,
+    }
+  }
+  componentDidMount() {
+    const { code } = this.props.location.query
+    axios.post('/api/auth',{
+      code,
+    }).then((res) => {
+      //Code was accepted, the token should be in the response data
+      const { token } = res.data
+      cookie.save('token', token)
+      location = '/'
+    }).catch((err) => {
+      console.error(err)
+      location = '/'
+    })
+  }
+  render() {
+    return null
+  }
 }
 
 export default Auth
