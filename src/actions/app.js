afab7152fd033060e33a404202465045e964910e
JMH <jheld135@gmail.com>
2017-03-08 11:00:29 -0600

    Added App reducer + tests

--- src/actions/app.js
+++ src/actions/app.js
@@ -2,7 +2,7 @@ export const SET_SNIPPET_CONTENTS = 'SET_SNIPPET_CONTENTS';
 export const SET_RULE_FILTERS = 'SET_RULE_FILTERS';
 export const SET_AST = 'SET_AST';
 export const TOGGLE_EDITING_STATE = 'TOGGLE_EDITING_STATE';
-export const ADD_ANNOTATION = 'ADD_ANNOTATION';
+export const SAVE_ANNOTATION = 'SAVE_ANNOTATION';
 export const EDIT_ANNOTATION = 'EDIT_ANNOTATION';
 export const RESTORE_STATE = 'RESTORE_STATE';
 
@@ -25,8 +25,8 @@ export const toggleEditState = () => ({
   type: TOGGLE_EDITING_STATE,
 });
 
-export const addAnnotation = (annotationData) => ({
-  type: ADD_ANNOTATION,
+export const saveAnnotation = (annotationData) => ({
+  type: SAVE_ANNOTATION,
   payload: annotationData,
 });
 
--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,6 +1,6 @@
 import * as actions from '../actions/app';
 
-const initialState = {
+export const initialState = {
   snippet: '',
   snippetTitle: '',
   annotations: {},
@@ -11,6 +11,36 @@ const initialState = {
 
 const AppReducer = (state = initialState, action) => {
   switch (action.type) {
+    case actions.SET_SNIPPET_CONTENTS: {
+      return Object.assign({}, state, {
+        snippet: action.payload,
+      });
+    }
+    case actions.SET_RULE_FILTERS: {
+      return Object.assign({}, state, {
+        filters: action.payload,
+      });
+    }
+    case actions.TOGGLE_EDITING_STATE: {
+      return Object.assign({}, state, {
+        readOnly: !state.readOnly,
+      });
+    }
+    case actions.SET_AST: {
+      return Object.assign({}, state, {
+        AST: action.payload,
+      });
+    }
+    case actions.SAVE_ANNOTATION: {
+      return Object.assign({}, state, {
+        annotations: Object.assign({}, state.annotations, {
+          [action.payload.lineNumber]: action.payload,
+        }),
+      });
+    }
+    case actions.RESTORE_STATE: {
+      return Object.assign({}, action.payload);
+    }
     default: {
       return state;
     }
