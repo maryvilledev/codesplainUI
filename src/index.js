1cf27a5256afe84d0dcbe35a8c124775aac06afd
Dane Johnson <dane_johnson@ymail.com>
2017-03-08 10:13:16 -0600

    moved App to containers folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './containers/App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
