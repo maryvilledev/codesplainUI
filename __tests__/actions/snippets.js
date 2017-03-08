9c39ee0096c7ad550b831df775f85ab96c23eb72
JMH <jheld135@gmail.com>
2017-03-08 08:37:44 -0600

    Added actions and greened all tests for them

--- __tests__/actions/snippets.js
+++ __tests__/actions/snippets.js
@@ -1,86 +1,40 @@
 import * as actions from '../../src/actions/snippets';
 
-describe('snippet actions', () => {
-  it('creates an action to set the snippet contents', () => {
-    const snippetContents = 'show me what you got';
-    const expected = {
-      type: "SET_SNIPPET_CONTENTS",
-      payload: snippetContents,
-    };
-    expect(actions.setSnippetContents(snippetContents)).toEqual(expected);
+describe('Actions: Snippets', () => {
+  describe('SET_SNIPPET_CONTENTS', () => {
+    it('creates an action to set the snippet contents', () => {
+      const snippet = 'Show me what you got';
+      const expected = {
+        type: actions.SET_SNIPPET_CONTENTS,
+        payload: snippet,
+      };
+      expect(actions.setSnippetContents(snippet)).toEqual(expected);
+    });
   });
-  it('creates an action to set the rule filters', () => {
-    const filters = {
-      'for_stmt': {
-        prettyTokenName: 'For Loops',
-        count: 1,
-        selected: false
-      },
-      'atom': {
-        prettyTokenName: 'Atoms',
-        count: 21,
-        selected: true
-      },
-    };
-    const expected = {
-      type: "SET_RULE_FILTERS",
-      payload: filters
-    };
-    expect(actions.setRuleFilters(filters)).toEqual(expected);
+  describe('SET_RULE_FILTERS', () => {
+    it('creates an action to set the rule filters', () => {
+      const filters = {
+        'for_stmt': {
+          prettyTokenName: 'For Loops',
+          count: 1,
+          selected: false
+        },
+        'atom': {
+          prettyTokenName: 'Atoms',
+          count: 21,
+          selected: true
+        },
+      };
+      const expected = {
+        type: actions.SET_RULE_FILTERS,
+        payload: filters
+      };
+      expect(actions.setRuleFilters(filters)).toEqual(expected);
+    });
   });
-  it('creates an action to set the abstract syntax tree', () => {
-    const AST = {
-      "type": "file_input",
-      "begin": 0,
-      "end": 1,
-      "children": [
-        "NEWLINE",
-        "INDENT",
-        "NEWLINE",
-        "DEDENT"
-      ],
-    };
-    const expected = {
-      type: "SET_AST",
-      payload: AST
-    };
-    expect(actions.setAST(AST)).toEqual(expected);
-  });
-  it('creates an action that toggles the editing state', () => {
-    const expected = {
-      type: "TOGGLE_EDITING_STATE",
-    };
-    expect(actions.toggleEditState()).toEqual(expected);
-  });
-  it('creates an action that adds an annotation', () => {
-    const annotationData = {
-      free: ['Frank'],
-      projects: ['Flood the School' : ['Donnie']]
-    };
-    const expected = {
-      type: "ADD_ANNOTATION",
-      payload: savedState
-    };
-    expect(actions.addAnnotation(savedState)).toEqual(expected);
-  });
-  it('creates an action that edit an annotation', () => {
-    const annotationData = {
-      annotation: 'some annotations',
-      lineNumber: 1,
-      lineText: 'some line from a code snippet',
-    };
-    const expected = {
-      type: "EDIT_ANNOTATION",
-      payload: savedState
-    };
-    expect(actions.editAnnotation(annotationData)).toEqual(expected);
-  });
-  it('creates an action that restores state', () => {
-    const savedState = {
-      "snippet": "\t",
-      "snippetTitle": "",
-      "annotations": {},
-      "AST": {
+  describe('SET_AST', () => {
+    it('creates an action to set the abstract syntax tree', () => {
+      const AST = {
         "type": "file_input",
         "begin": 0,
         "end": 1,
@@ -89,15 +43,76 @@ describe('snippet actions', () => {
           "INDENT",
           "NEWLINE",
           "DEDENT"
-        ]
-      },
-      "filters": {},
-      "readOnly": false
-    };
-    const expected = {
-      type: "RESTORE_STATE",
-      payload: savedState
-    };
-    expect(actions.restoreState(savedState)).toEqual(expected);
+        ],
+      };
+      const expected = {
+        type: actions.SET_AST,
+        payload: AST
+      };
+      expect(actions.setAST(AST)).toEqual(expected);
+    });
+  });
+  describe('TOGGLE_EDITING_STATE', () => {
+    it('creates an action that toggles the editing state', () => {
+      const expected = {
+        type: actions.TOGGLE_EDITING_STATE,
+      };
+      expect(actions.toggleEditState()).toEqual(expected);
+    });
+  });
+  describe('ADD_ANNOTATION', () => {
+    it('creates an action that adds an annotation', () => {
+      const annotationData = {
+        annotation: 'You pass butter.',
+        lineNumber: 1,
+        lineText: 'What is my purpose?'
+      };
+      const expected = {
+        type: actions.ADD_ANNOTATION,
+        payload: annotationData,
+      };
+      expect(actions.addAnnotation(annotationData)).toEqual(expected);
+    });
+  });
+  describe('EDIT_ANNOTATION', () => {
+    it('creates an action that edit an annotation', () => {
+      const annotationData = {
+        annotation: 'You pass butter.',
+        lineNumber: 1,
+        lineText: 'What is my purpose?',
+      };
+      const expected = {
+        type: actions.EDIT_ANNOTATION,
+        payload: annotationData
+      };
+      expect(actions.editAnnotation(annotationData)).toEqual(expected);
+    });
+  });
+  describe('RESTORE_STATE', () => {
+    it('creates an action that restores state', () => {
+      const savedState = {
+        "snippet": "\t",
+        "snippetTitle": "",
+        "annotations": {},
+        "AST": {
+          "type": "file_input",
+          "begin": 0,
+          "end": 1,
+          "children": [
+            "NEWLINE",
+            "INDENT",
+            "NEWLINE",
+            "DEDENT"
+          ]
+        },
+        "filters": {},
+        "readOnly": false
+      };
+      const expected = {
+        type: actions.RESTORE_STATE,
+        payload: savedState
+      };
+      expect(actions.restoreState(savedState)).toEqual(expected);
+    });
   });
 });
