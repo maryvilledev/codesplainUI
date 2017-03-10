ffe49470a0a189705f84fbb472310c8e3857dc99
Dane Johnson <dane_johnson@ymail.com>
2017-03-10 13:55:02 -0600

    further improved on enzyme test

--- __tests__/containers/FilterArea.js
+++ __tests__/containers/FilterArea.js
@@ -3,7 +3,6 @@ import { shallow, mount, render } from 'enzyme'
 
 import { FilterArea } from '../../src/containers/FilterArea'
 
-let mockOnRuleSelected;
 const mockFilters = {
   rick: {
     selected: false,
@@ -16,29 +15,17 @@ const mockFilters = {
 }
 const mockDispatch = jest.fn()
 
-//Mock to simply render children
-jest.mock('material-ui', () => ({
-  CardText: ({children}) => children
-}))
-//Mock to allow me to arbitrarily toggle
-jest.mock('../../src/components/RulesSelector', () => {
-  return jest.fn(({onRuleSelected}) => {
-    mockOnRuleSelected = onRuleSelected
-    return null
-  })
-})
-
 describe('<FilterArea />', () => {
   const filterToToggle = 'rick';
   const filterNotToToggle = 'morty'
-  it('toggles a filter when handleSaveAnnotation is called', () => {
-    const wrapper = mount(
+  it('toggles a filter when handleRuleSelected is called', () => {
+    const wrapper = shallow(
       <FilterArea
         filters={mockFilters}
         dispatch={mockDispatch}
       />
     )
-    mockOnRuleSelected(filterToToggle)
+    wrapper.instance().handleRuleSelected(filterToToggle)
     const { payload } = mockDispatch.mock.calls[0][0];
     expect(payload[filterToToggle].selected).toBe(true);
     expect(payload[filterNotToToggle].selected).toBe(false);
