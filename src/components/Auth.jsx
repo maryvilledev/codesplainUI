ed19c9a83db85ee2f6dd756a7d5dd809e1928668
Hopding <andrew.dillon.j@gmail.com>
2017-03-09 20:44:54 -0600

    Added documentation; Added a cookie to cookie to hold user's location to redirect user to after they login'

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -3,38 +3,52 @@ import axios from 'axios'
 import cookie from 'react-cookie'
 import { CircularProgress } from 'material-ui'
 
-//Code is passed in from Github
+/*
+<Auth /> is the component that is rendered for the '{{url}}/auth' endpoint (which
+is the endpoint the user is sent to after signing in to GitHub). It initially
+renders as a <CircularProgress /> and sends a POST request to the express backend
+to complete the OAuth authentication with GitHub. Upon completion of this, the
+user is redirected to the URL they attempted to login from initially.
+*/
 class Auth extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      waiting: true
-    }
+  constructor(props) {
+    super(props);
+    this.state = { waiting: true };
   }
+
   componentDidMount() {
+    // Extract the code provided by GitHub from the redirect query string
     const { code } = this.props.location.query
-    axios.post('/api/auth',{
-      code,
-    }).then((res) => {
-      //Code was accepted, the token should be in the response data
-      const { token } = res.data
-      cookie.save('token', token, {path: '/'})
-      this.setState({waiting: false})
-    }).catch((err) => {
-      console.error(err)
-      this.setState({waiting: false})
-    })
+
+    // Post it to the express backend to be verified by GitHub as authentic
+    axios.post('/api/auth', { code })
+      .then(res => {
+        // Code was accepted, so extract and save the token from the response
+        const { token } = res.data;
+        cookie.save('token', token, { path: '/' });
+        this.setState({ waiting: false });
+      })
+      .catch((err) => {
+        // Something went wrong ;(
+        console.error(err);
+        this.setState({ waiting: false });
+      });
+
+    // Load the URL to redirect user to
+    const signInRedirect = cookie.load('signInRedirect');
+    if (signInRedirect)
+      this.setState({ signInRedirect });
+    else
+      this.setState({ signInRedirect: '/' });
   }
+
   render() {
-    const { waiting } = this.state
-    if( waiting ) {
-      return (
-        <CircularProgress size={100} thickness={7} />
-      )
-    } else {
-      window.location = '/'
-      return null
-    }
+    // Conditionally render a <CircularProgress /> or redirect user, depending
+    // on whether the backend has responded yet
+    if (this.state.waiting)
+      return <CircularProgress size={100} thickness={7} />
+    else
+      window.location = this.state.signInRedirect;
   }
 }
 
--- src/components/buttons/LoginButton.jsx
+++ src/components/buttons/LoginButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FlatButton } from 'material-ui';
 import GitHubIcon from '../../../res/github-icon.png';
+import cookie from 'react-cookie'
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
 const GITHUB_URL = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`
@@ -17,6 +18,13 @@ const styles = {
   },
 }
 
+// Saves a cookie containing the current URL path, so we can retrieve it
+// later when GitHub sends the user back to the app, and send the user back
+// to the page they were on when they clicked this button.
+const saveSignInRedirect = () => {
+  cookie.save('signInRedirect', location.pathname, { path: '/' });
+}
+
 /*
 <LoginButton /> renders as white clickable "LOG IN" text, with a white octocat
 icon to its left. When clicked, the button sends the user to the GitHub OAuth 
@@ -25,6 +33,7 @@ authorization URL, where the client ID is set to the CLIENT_ID env var.
 const LoginButton = () => (
   <FlatButton 
     href={GITHUB_URL}
+    onClick={saveSignInRedirect}
     style={styles.button}
     label={
       <span style={styles.span}>
