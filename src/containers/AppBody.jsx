ce5662e059c0a38fbcb9d5ccc5bad3d43e2e12cf
Dane Johnson <dane_johnson@ymail.com>
2017-03-10 09:51:57 -0600

    Added ability to restore state

---
+++ src/containers/AppBody.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Card } from 'material-ui';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+
+import { restoreState } from '../actions/app';
+
+import Annotations from './Annotations';
+import FilterArea from './FilterArea';
+import SnippetArea from './SnippetArea';
+
+class AppBody extends React.Component {
+  componentDidMount() {
+      const { id } = this.props.params;
+      const { dispatch } = this.props;
+      if (!id) return;
+      axios.get(`/api/snippets/${id}`)
+        .then(res => {
+          const stateString = res.data.json;
+          const obj = JSON.parse(stateString);
+          dispatch(restoreState(obj));
+        })
+        .catch(err => {
+          // Bad URL, redirect
+          browserHistory.push('/');
+        });
+  }
+  render() {
+    return (
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-md-2'>
+            <Card><FilterArea /></Card>
+          </div>
+          <div className='col-md-5'>
+            <Card><SnippetArea /></Card>
+          </div>
+          <div className='col-md-5'>
+            <Card><Annotations /></Card>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default connect()(AppBody)
