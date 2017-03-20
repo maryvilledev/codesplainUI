7e0e6eb12e6b2654107f56f88c0f57c7472e9152
Dane Johnson <dane_johnson@ymail.com>
2017-03-20 10:07:23 -0500

    Replaced hand-rolled index-to-pos function with CodeMirror's implementation

--- src/util/codemirror-utils.js
+++ src/util/codemirror-utils.js
@@ -52,9 +52,7 @@ start and end are inclusive.
 */
 export function styleRegion(codeMirror, start, end, css) {
   if (end < start) throw new Error('end must be greater than start');
-  const snippet = codeMirror.getValue();
-  const convert = getIndexToRowColConverter(snippet);
-  codeMirror.markText(convert(start), convert(end), {css: css});
+  codeMirror.markText(codeMirror.posFromIndex(start), codeMirror.posFromIndex(end), {css: css});
 }
 /*
 Given a CodeMirror instance styleLine() will apply the specified css style to the
@@ -72,29 +70,6 @@ code in the editor.
 */
 export function styleAll(codeMirror, css) {
   styleRegion(codeMirror, 0, codeMirror.getValue().length, css);
-}
-
-/*
-Returns a function that will convert a character index to an object
-of its row and column in the context of the passed-in snippet.
-
-The returned object is of the following form: { line: <int>, ch: <int> }
-
-Regards the first row in a snippet as row 0, and the first column of each
-row as column 0.
-*/
-export function getIndexToRowColConverter(snippet) {
-  // Make array containing the length of each lines of code in snippet
-  const lines = snippet.split('\n').map(l => l.length + 1); // +1 to accomodate '\n' that was lost when we split the snippet
-  const converter = (index) => {
-    let row, col, lenPrevRows;
-    row = lenPrevRows = 0;
-    while (index > lines[row] + lenPrevRows)
-      lenPrevRows += lines[row++];
-    col = index - lenPrevRows;
-    return { line: row, ch: col }
-  }
-  return _.memoize(converter)
 }
 
 /*
