d67100dc9deaa8b5fb7c1a51e1cf6011f8b5ae3c
JMH <jheld135@gmail.com>
2017-03-20 11:25:47 -0500

    Refactor tests for CodesplainAppBar component

--- __tests__/components/CodesplainAppBar.test.jsx
+++ __tests__/components/CodesplainAppBar.test.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cookie from 'react-cookie';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -9,10 +10,23 @@ describe('<CodesplainAppBar />', () => {
   const muiTheme = getMuiTheme();
   const shallowWithContext = (node) => shallow(node, { context: { muiTheme } });
 
-  it('matches snapshot', () => {
-    const wrapper = shallowWithContext(
-      <CodesplainAppBar />
-    );
-    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  describe('snapshot tests', () => {
+    afterEach(() => {
+      cookie.remove('token');
+    });
+    it('user is not logged in', () => {
+      const wrapper = shallowWithContext(
+        <CodesplainAppBar />
+      );
+      expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('user is logged in', () => {
+      cookie.save('token', 'foobar')
+      const wrapper = shallowWithContext(
+        <CodesplainAppBar />
+      );
+      expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
   });
 });
