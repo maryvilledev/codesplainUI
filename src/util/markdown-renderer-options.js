eb1312e5d0d128f92388ecc812cd4e68e7e1d54a
JMH <jheld135@gmail.com>
2017-03-22 12:10:16 -0500

    Remove console logs and refactored highlight function

--- src/util/markdown-renderer-options.js
+++ src/util/markdown-renderer-options.js
@@ -3,17 +3,13 @@ import hljs from 'highlight.js';
 export default {
   langPrefix: 'hljs language-',
   highlight: function (str, lang) {
-    if (lang && hljs.getLanguage(lang)) {
-      try {
-        console.log('foo');
-        return hljs.highlight(lang, str).value;
-      } catch (err) {}
-    }
-    console.log('bar');
     try {
+      if (lang && hljs.getLanguage(lang)) {
+        return hljs.highlight(lang, str).value;
+      }
       return hljs.highlightAuto(str).value;
     } catch (err) {}
-    console.log('baz');
+
     return ''; // use external default escaping
   }
 }
