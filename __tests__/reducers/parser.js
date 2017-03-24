91911471b98b8dc4e2c5e383349b424a34f69c6e
Dane Johnson <dane_johnson@ymail.com>
2017-03-24 13:26:55 -0500

    added tests for parser reducer

---
+++ __tests__/reducers/parser.js
@@ -0,0 +1,36 @@
+import * as actions from '../../src/actions/parser';
+import reducer from '../../src/reducers/parser';
+
+describe('Reducer: Parser', () => {
+  it('should have initial state', () => {
+    const initial = {
+      state: actions.LOADING,
+      parser: null
+    }
+    expect(reducer(undefined, {})).toEqual(initial)
+  })
+
+  it('should handle SET_PARSER_STATE', () => {
+    const nextState = actions.LOADED;
+    action = {
+      type: actions.SET_PARSER_STATE,
+      payload: nextState
+    };
+    const expected = {
+      state: actions.LOADED
+    };
+    expect(reducer(undefined, action)).toEqual(expect.objectContaining(expected))
+  })
+
+  it('should handle LOAD_PARSER', () => {
+    const parser = jest.fn()
+    action = {
+      type: actions.LOAD_PARSER,
+      payload: parser
+    };
+    const expected = {
+      parser: parser
+    };
+    expect(reducer(undefined, action)).toEqual(expect.objectContaining(expected))
+  })
+})
