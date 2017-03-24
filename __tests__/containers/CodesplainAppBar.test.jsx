7a14d8d0d1e03d8c7ef3b628db012dea181e15d6
Hopding <andrew.dillon.j@gmail.com>
2017-03-24 08:26:41 -0500

    Updated tests

---
+++ __tests__/containers/CodesplainAppBar.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import cookie from 'react-cookie';
+import { shallow } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { createStore } from 'redux';
+import app from '../../src/reducers/app.js'
+
+import CodesplainAppBar from '../../src/containers/CodesplainAppBar';
+
+describe('<CodesplainAppBar />', () => {
+  const muiTheme = getMuiTheme();
+  const shallowWithContext = (node) => shallow(node, { context: { muiTheme } });
+
+  describe('snapshot tests', () => {
+    afterEach(() => {
+      cookie.remove('token');
+    });
+    it('user is not logged in', () => {
+      const wrapper = shallowWithContext(
+        <CodesplainAppBar 
+          store={createStore(app)}
+        />
+      );
+      expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('user is logged in', () => {
+      cookie.save('token', 'foobar')
+      const wrapper = shallowWithContext(
+        <CodesplainAppBar 
+          store={createStore(app)}
+        />
+      );
+      expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+  });
+});
