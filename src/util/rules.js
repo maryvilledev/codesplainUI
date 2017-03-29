6b3226a61ae2f2f56e41349a1dc74e3f5a7a7ea6
JMH <jheld135@gmail.com>
2017-03-29 10:45:24 -0500

    Add break to rules list

--- src/util/rules.js
+++ src/util/rules.js
@@ -1,10 +1,12 @@
 /* Object mapping token types to pretty names and colors for display */
 export const rules = {
-  'arith_expr': { prettyName: 'Arithmetic Expression', color: '#FFA500' },
+  // Non-terminal rules
   'and_expr': { prettyName: 'And', color: '#F0E68C' },
   'argument': { prettyName: 'Arguments', color: '#EAF7AB' },
+  'arith_expr': { prettyName: 'Arithmetic Expression', color: '#FFA500' },
   'atom':     { prettyName: 'Atoms', color: '#ABDBF7' },
   'augassign':   { prettyName: 'Augmented Assignment', color: '#00ffff' },
+  'break_stmt': { prettyName: 'Break Statement', color: '#00ffa0' },
   'classdef': { prettyName: 'Class Definitions', color: '#03C03C' },
   'comp_op':  { prettyName: 'Comparison Operator', color: '#DAA520' },
   'dictorsetmaker': { prettyName: 'Dictionary', color: '#00FF7F' },
@@ -22,6 +24,8 @@ export const rules = {
   'str':      { prettyName: 'Strings', color: '#CAABF7' },
   'try_stmt': { prettyName: 'Try Statements', color: '#966FD6' },
   'while_stmt':  { prettyName: 'While Loops', color: '#F0E68C' },
+
+  // Terminal rules
   '.TRUE': { prettyName: 'Boolean (True)', color: '#ff9933' },
   '.FALSE': { prettyName: 'Boolean (False)', color: '#e67300' },
 }
