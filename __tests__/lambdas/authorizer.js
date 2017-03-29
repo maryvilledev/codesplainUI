84f249f37b7d1613adfe49892f335b32b61767b7
Dane Johnson <dane_johnson@ymail.com>
2017-03-29 08:25:58 -0500

    Revert "Removed test"
    This reverts commit 95da4858a529dbc1649f67f5625255769e59b46e.

---
+++ __tests__/lambdas/authorizer.js
@@ -0,0 +1,68 @@
+const mockClientId = process.env.CLIENT_ID
+
+import { handler } from '../../awslambda/authorizer'
+
+const mockMethodArn = 'Teleport'
+
+const mockValidToken = 'ricksanchez'
+const mockInvalidToken = 'mortysmith'
+
+jest.mock('axios', () => {
+  return {
+    errored: true,
+    get: function (url, opts) {
+      const valid = (url ===
+        `https://api.github.com/applications/${mockClientId}/tokens/${mockValidToken}`)
+      if (valid) {
+        this.errored = false
+      }
+      return this
+    },
+    then: function(callback) {
+      if(!this.errored) {
+        callback()
+      }
+      return this;
+    },
+    catch: function(callback) {
+      if(this.errored) {
+        callback()
+      }
+    }
+  }
+})
+jest.mock('aws-sdk')
+
+describe('handler', () => {
+  it('should create a policy if a valid token is passed', () => {
+    const mockEvent = {
+      methodArn: mockMethodArn,
+      authorizationToken: mockValidToken
+    }
+    const expected = {
+      principalId: 'user',
+      policyDocument: expect.objectContaining({
+        Statement: [{
+          Action: 'execute-api:Invoke',
+          Effect: 'Allow',
+          Resource: mockMethodArn
+        }]
+      })
+    }
+    const mockCallback = (error, policy) => {
+      expect(error).toBe(null);
+      expect(policy).toEqual(expected);
+    }
+    handler(mockEvent, null, mockCallback)
+  })
+  it('should return "Unauthorized" if token is not valid', () => {
+    const mockEvent = {
+      methodArn: mockMethodArn,
+      authorizationToken: mockInvalidToken
+    }
+    const expected = "Unauthorized"
+    const mockCallback = (error, policy) => {
+      expect(error).toEqual(expected);
+    }
+  })
+})
