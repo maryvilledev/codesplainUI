cf957ed2154793510094bbe152904d962c2c01dd
JMH <jheld135@gmail.com>
2017-04-06 11:43:08 -0500

    Refactor atom rule-type to be included in ignoredRules

--- src/util/rules.js
+++ src/util/rules.js
@@ -4,7 +4,6 @@ export const rules = {
   'and_expr': { prettyName: 'And', color: '#F0E68C' },
   'argument': { prettyName: 'Arguments', color: '#EAF7AB' },
   'arith_expr': { prettyName: 'Arithmetic Expression', color: '#FFA500' },
-  'atom':     { prettyName: 'Atoms', color: '#ABDBF7' },
   'augassign':   { prettyName: 'Augmented Assignment', color: '#00ffff' },
   'break_stmt': { prettyName: 'Break Statement', color: '#00ffa0' },
   'classdef': { prettyName: 'Class Definitions', color: '#03C03C' },
@@ -31,6 +30,7 @@ export const rules = {
 
 /* Array of rules produced by parser, but ignored by the UI layer */
 export const ignoredRules = [
+  'atom',
   'suite',
   'file_input',
   'simple_stmt',
