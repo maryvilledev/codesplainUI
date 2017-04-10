890545f8140f4929e8013cad079305042270d4e2
Dane Johnson <dane_johnson@ymail.com>
2017-04-10 14:17:04 -0500

    added icons

--- src/components/ReferenceArea.jsx
+++ src/components/ReferenceArea.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
-import { Paper } from 'material-ui'
 
 import mtIcon from '../../res/mt.png'
+import twitterIcon from '../../res/twitter.png'
+import githubIcon from '../../res/github-icon-blue.png'
 
 const style = {
   paper: {
     position: 'fixed',
     bottom: 0,
     right: 0,
-    padding: '1%',
   },
   linkArea: {
     display: 'inline-block',
+    font: '12pt Lato, sans-serif',
   },
   link: {
     padding: '4px',
+    color: '#00BCD4',
   }
 }
 
 const ReferenceArea = () => (
-    <Paper style={style.paper}>
+    <div style={style.paper}>
       <a href="https://maryvilledevcenter.com/">
       <img
         src={mtIcon}
@@ -27,13 +29,23 @@ const ReferenceArea = () => (
         width={80}
       />
       </a>
-      <div style={style.linkArea}>
-      <a
-        href="https://github.com/maryvilledev/codesplain/issues"
-        style={style.link}
-      >
-        Report bug
+      <a href="https://www.twitter.com/MaryvilleTech"
+      style={style.link}>
+        <img
+          src={twitterIcon}
+          alt="Twitter Icon"
+          width={30}
+        />
       </a>
+      <a href="https://github.com/maryvilledev"
+      style={style.link}>
+        <img
+          src={githubIcon}
+          alt="Github Icon"
+          width={30}
+        />
+      </a>
+      <div style={style.linkArea}>
       <a
         href="mailto://support.maryville.com"
         style={style.link}
@@ -41,7 +53,7 @@ const ReferenceArea = () => (
           Contact Us
         </a>
       </div>
-    </Paper>
+    </div>
 );
 
 export default ReferenceArea;
