6a1ff158e9e5085bcc49d4e494ff1513eade382b
Dane Johnson <dane_johnson@ymail.com>
2017-04-10 11:51:37 -0500

    first pass

--- src/components/ReferenceArea.jsx
+++ src/components/ReferenceArea.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import { Paper } from 'material-ui'
+
+import mtIcon from '../../res/mt.png'
 
 const style = {
   area: {
-    position: 'fixed'
+    position: 'fixed',
+    bottom: 0,
+    right: 0
+  },
+  paper: {
+    padding: '3%'
   }
 }
 
 const ReferenceArea = () => (
-  <span style={style.area}>Hi</span>
+  <span style={style.area}>
+    <Paper style={style.paper}>
+      <a href="https://maryvilledevcenter.com/">
+      <img
+        src={mtIcon}
+        alt="Maryville DevCenter Logo"
+        width={80}
+      />
+      </a>
+      <a href="https://github.com/maryvilledev/codesplain/issues">
+        Report bug
+      </a>
+      <a href="mailto://support.maryville.com">Contact Us</a>
+    </Paper>
+  </span>
 );
 
 export default ReferenceArea;
