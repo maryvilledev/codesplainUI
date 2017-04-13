cd98371331139943d1adc46816f9e05a8d045bd1
JMH <jheld135@gmail.com>
2017-04-13 16:43:12 -0500

    Double -> single quotes

--- src/util/parser-error-logger.js
+++ src/util/parser-error-logger.js
@@ -5,7 +5,7 @@ const {
 
 let errorFn;
 
-if (NODE_ENV === 'development' && REACT_APP_ENABLE_PARSER_DEBUGGING === "true") {
+if (NODE_ENV === 'development' && REACT_APP_ENABLE_PARSER_DEBUGGING === 'true') {
   errorFn = (err) => { console.error(err); };
 } else {
   errorFn = () => {};
