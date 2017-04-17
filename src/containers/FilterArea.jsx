b5c089f85c2a73038e44c359a530e793e256f78d
JMH <jheld135@gmail.com>
2017-04-17 16:34:44 -0500

    Fix linter issues in FilterArea

--- src/containers/FilterArea.jsx
+++ src/containers/FilterArea.jsx
@@ -1,53 +1,53 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import _ from 'lodash'
+import React from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
 import {
   Card,
   CardText,
   CardTitle,
 } from 'material-ui';
 
-import FilterAreaActions from '../components/FilterAreaActions';
-import RulesSelector from '../components/RulesSelector';
-
 import {
   resetFilters,
   selectAllFilters,
-  setRuleFilters
+  setRuleFilters,
 } from '../actions/app';
-
-const toggleFilter = (filters, filterName) => {
-  filters[filterName].selected = !filters[filterName].selected
-}
+import FilterAreaActions from '../components/FilterAreaActions';
+import RulesSelector from '../components/RulesSelector';
+import CustomPropTypes from '../util/custom-prop-types';
 
 export class FilterArea extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.handleRuleSelected = this.handleRuleSelected.bind(this);
-    this.handleClearAllFilters = this.handleClearAllFilters.bind(this);;
+    this.handleClearAllFilters = this.handleClearAllFilters.bind(this);
     this.handleSelectAllFilters = this.handleSelectAllFilters.bind(this);
   }
+
   handleRuleSelected(filterName) {
-    const { dispatch, filters } = this.props
-    const newFilters = _.cloneDeep(filters)
-    toggleFilter(newFilters, filterName)
-    dispatch(setRuleFilters(newFilters))
+    const { dispatch, filters } = this.props;
+    const newFilters = _.cloneDeep(filters);
+    newFilters[filterName].selected = !newFilters[filterName].selected;
+    dispatch(setRuleFilters(newFilters));
   }
+
   handleClearAllFilters() {
     const { dispatch } = this.props;
     dispatch(resetFilters());
   }
+
   handleSelectAllFilters() {
     const { dispatch } = this.props;
     dispatch(selectAllFilters());
   }
+
   render() {
     const { filters } = this.props;
     const filterAreaActions = Object.keys(filters).length ?
-      <FilterAreaActions
+      (<FilterAreaActions
         clearAll={this.handleClearAllFilters}
         selectAll={this.handleSelectAllFilters}
-      /> : null;
+      />) : null;
     return (
       <Card>
         <CardTitle title="Filters" />
@@ -63,7 +63,11 @@ export class FilterArea extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => ({
+FilterArea.propTypes = {
+  filters: CustomPropTypes.filters.isRequired,
+};
+
+const mapStateToProps = state => ({
   filters: state.app.filters,
 });
 
