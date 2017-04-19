386b24b4d3df18bf8612c9d4b86910fc4cad3985
Dane Johnson <dane_johnson@ymail.com>
2017-04-19 10:03:09 -0500

    updated reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -7,6 +7,28 @@ const defaultState = {
 
 const user = (state=defaultState, action) => {
   switch (action.type) {
+    case actions.ADD_ORG: {
+      const org = action.payload;
+      if(state.orgs.includes(org)) {
+        return state;
+      } else {
+        return {
+          ...state,
+          orgs: state.orgs.slice().concat(org)
+        };
+      }
+    }
+    case actions.SWITCH_ORG: {
+      const org = action.payload;
+      if (state.orgs.includes(org)) {
+        return {
+          ...state,
+          selectedOrg: org
+        };
+      } else {
+        return state;
+      }
+    }
     case actions.SET_USER_SNIPPETS: {
       return {
         ...state,
