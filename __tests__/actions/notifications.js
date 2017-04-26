4644f63d3704e6bf05259df0e08cc24a483e7696
JMH <jheld135@gmail.com>
2017-04-26 09:09:00 -0500

    Convert action creator tests to snapshot tests

--- __tests__/actions/notifications.js
+++ __tests__/actions/notifications.js
@@ -4,19 +4,12 @@ describe('Actions: notifications:', () => {
   describe('addNotification', () => {
     it('creates correct action object', () => {
       const notification = 'Wubba lubba dub dub!';
-      const expected = {
-        type: actions.ADD_NOTIFICATION,
-        payload: notification,
-      };
-      expect(actions.addNotification(notification)).toEqual(expected);
+      expect(actions.addNotification(notification)).toMatchSnapshot();
     });
   });
   describe('closeNotification', () => {
     it('creates correct action object', () => {
-      const expected = {
-        type: actions.CLOSE_NOTIFICATION,
-      };
-      expect(actions.closeNotification()).toEqual(expected);
+      expect(actions.closeNotification()).toMatchSnapshot();
     });
   });
 });
