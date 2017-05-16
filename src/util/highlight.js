f947b9065d3d66246dd565bb12191d66347d80e5
JMH <jheld135@gmail.com>
2017-05-16 10:46:35 -0500

    Add Golang to highlight.js

--- src/util/highlight.js
+++ src/util/highlight.js
@@ -3,10 +3,12 @@
 import hljs from 'highlight.js/lib/highlight';
 
 // Each language needs to be imported like so:
+import go from 'highlight.js/lib/languages/go';
 import java from 'highlight.js/lib/languages/java';
 import python from 'highlight.js/lib/languages/python';
 
 // Register each language to the hljs instance
+hljs.registerLanguage('go', go);
 hljs.registerLanguage('java', java);
 hljs.registerLanguage('python', python);
 
