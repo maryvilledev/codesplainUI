dbd001150bc77a24febbcd6018eed01ab42c7475
JMH <jheld135@gmail.com>
2017-05-18 14:37:56 -0500

    Update defaultStateValues to fix issue with loading incomplete snippets

--- src/util/state-management.js
+++ src/util/state-management.js
@@ -4,6 +4,11 @@ export const defaultStateValues = {
   // Default values for state should go here. If a value was already set
   // by state, it won't be overriden by this object
   snippetLanguage: 'python3',
+  annotations: {},
+  snippet: '',
+  readOnly: false,
+  snippetTitle: 'title',
+  filters: {},
 };
 
 export const setDefaults = state => _.defaults(
