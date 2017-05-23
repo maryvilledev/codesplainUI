5d026322cd9461a654ff95c7c41a2817c7d30a95
JMH <jheld135@gmail.com>
2017-05-23 14:06:40 -0500

    Add util to import all Codemirror themes

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,7 +1,6 @@
 import React, { PropTypes } from 'react';
 import CodeMirror from 'react-codemirror';
 import isEqual from 'lodash/isEqual';
-import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/python/python';
 
 import {
@@ -11,8 +10,8 @@ import {
   styleAll,
   styleRegion,
 } from '../util/codemirror-utils';
+import '../util/codemirror-themes';
 import CustomPropTypes from '../util/custom-prop-types';
-import '../styles/codesplain.css';
 
 // Options for the CodeMirror instance that are shared by edit and annotation mddes
 const baseCodeMirrorOptions = {
@@ -208,6 +207,7 @@ class Editor extends React.Component {
 }
 
 Editor.propTypes = {
+  errors: CustomPropTypes.errors,
   filters: CustomPropTypes.filters.isRequired,
   language: PropTypes.string.isRequired,
   markedLines: PropTypes.arrayOf(PropTypes.number).isRequired,
@@ -216,7 +216,6 @@ Editor.propTypes = {
   openLine: PropTypes.number,
   readOnly: PropTypes.bool.isRequired,
   value: PropTypes.string.isRequired,
-  errors: CustomPropTypes.errors,
 };
 
 Editor.defaultProps = {
